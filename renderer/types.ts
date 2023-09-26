a8e52cc8cd2c346239288cf0d390f475af308b99
Romuald Brillout <vike@brillout.com>
2023-09-26 11:15:51 +0200

    Scaffold Vike app

---
+++ renderer/types.ts
@@ -0,0 +1,35 @@
+export type { PageContextServer }
+export type { PageContextClient }
+export type { PageContext }
+export type { PageProps }
+
+import type {
+  PageContextBuiltInServer,
+  /*
+  // When using Client Routing https://vike.dev/clientRouting
+  PageContextBuiltInClientWithClientRouting as PageContextBuiltInClient
+  /*/
+  // When using Server Routing
+  PageContextBuiltInClientWithServerRouting as PageContextBuiltInClient
+  //*/
+} from 'vike/types'
+
+type Page = (pageProps: PageProps) => React.ReactElement
+type PageProps = Record<string, unknown>
+
+export type PageContextCustom = {
+  Page: Page
+  pageProps?: PageProps
+  urlPathname: string
+  exports: {
+    documentProps?: {
+      title?: string
+      description?: string
+    }
+  }
+}
+
+type PageContextServer = PageContextBuiltInServer<Page> & PageContextCustom
+type PageContextClient = PageContextBuiltInClient<Page> & PageContextCustom
+
+type PageContext = PageContextClient | PageContextServer
